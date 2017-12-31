80b1198b506102353a13629c2df832a68f93612d
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2017-12-31 11:26:05 +0000

    random colors on the characters

--- app/src/main/java/cesarferreira/lettersandnumbers/ContentFragment.kt
+++ app/src/main/java/cesarferreira/lettersandnumbers/ContentFragment.kt
@@ -1,8 +1,10 @@
 package cesarferreira.lettersandnumbers
 
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.AppCompatTextView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +14,11 @@ import java.util.*
 
 class ContentFragment : Fragment() {
 
-    private lateinit var randomGenerator: Random
     private lateinit var mItems: ArrayList<String>
+    private var previousCharacter: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        randomGenerator = Random()
 
         if (arguments != null) {
             mItems = arguments!!.getStringArrayList(ARG_PARAM1)
@@ -32,19 +32,44 @@ class ContentFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setRandomCharacter()
+        setRandomCharacter(characterTextView)
 
         characterTextView.setOnClickListener {
-            setRandomCharacter()
+            setRandomCharacter(characterTextView)
         }
     }
 
-    private fun setRandomCharacter() {
-        characterTextView.text = anyItem()
+    private fun setRandomCharacter(textView: AppCompatTextView) {
+        var randomChar: String
+
+        if (previousCharacter.isNullOrEmpty()) {
+            randomChar = getRandomCharacter()
+        } else {
+            do {
+                randomChar = getRandomCharacter()
+            } while (randomChar == previousCharacter)
+        }
+        previousCharacter = randomChar
+
+        textView.text = randomChar
+
+        setRandomColorToTextView(textView)
     }
 
-    private fun anyItem(): String {
-        return mItems[randomGenerator.nextInt(mItems.size)]
+    private fun getRandomCharacter(): String {
+        return mItems[Random().nextInt(mItems.size)]
+    }
+
+    private fun setRandomColorToTextView(textView: AppCompatTextView) {
+        textView.setTextColor(getRandomColor())
+    }
+
+    private fun getRandomColor(): Int {
+        val rnd = Random()
+        return Color.argb(255,
+                rnd.nextInt(200), // R
+                rnd.nextInt(200), // G
+                rnd.nextInt(200)) // B
     }
 
     companion object {
--- app/src/main/java/cesarferreira/lettersandnumbers/MainActivity.kt
+++ app/src/main/java/cesarferreira/lettersandnumbers/MainActivity.kt
@@ -7,44 +7,51 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_numbers -> {
-                switchToNumbersFragment()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_vowels -> {
-                switchToVowelsFragment()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_all_letters -> {
-                switchToAllLettersFragment()
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_numbers -> {
+                    switchToNumbersFragment()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_vowels -> {
+                    switchToVowelsFragment()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_all_letters -> {
+                    switchToLettersFragment()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
 
         switchToNumbersFragment()
     }
 
+    /**
+     * Switch to the Numbers fragment
+     */
     private fun switchToNumbersFragment() {
         val array: ArrayList<String> = arrayListOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
         replaceFragmentWith(ContentFragment.newInstance(array))
     }
 
+    /**
+     * Switch to the Vowels fragment
+     */
     private fun switchToVowelsFragment() {
         val array: ArrayList<String> = arrayListOf("A", "E", "I", "O", "U")
         replaceFragmentWith(ContentFragment.newInstance(array))
     }
 
-    private fun switchToAllLettersFragment() {
+    /**
+     * Switch to the Letters fragment
+     */
+    private fun switchToLettersFragment() {
         val array: ArrayList<String> = arrayListOf("A", "B", "C", "D", "E", "F", "G", "H", "I",
                 "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "W", "Y", "Z")
         replaceFragmentWith(ContentFragment.newInstance(array))
